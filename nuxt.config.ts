de5e06e73a895bb235a8c15c47f5e68e4bd351db
csipkesz <diesellucio@gmail.com>
2026-08-07 21:32:00 +0200

    feat: add icon serverBundle with new collections to enhance icon management

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/icon', '@nuxt/ui'],
-  css: ['~/assets/css/main.css'],
-  app: {
-    head: {
-      link: [
-        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
-        {
-          rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css2?family=Bodoni+Moda:ital,opsz,wght@0,6..96,400;0,6..96,500;1,6..96,500&family=Inter:wght@400;500;600&display=swap',
+    compatibilityDate: '2025-07-15',
+    devtools: {enabled: true},
+    modules: ['@nuxt/icon', '@nuxt/ui'],
+    css: ['~/assets/css/main.css'],
+    app: {
+        head: {
+            link: [
+                {rel: 'preconnect', href: 'https://fonts.googleapis.com'},
+                {rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: ''},
+                {
+                    rel: 'stylesheet',
+                    href: 'https://fonts.googleapis.com/css2?family=Bodoni+Moda:ital,opsz,wght@0,6..96,400;0,6..96,500;1,6..96,500&family=Inter:wght@400;500;600&display=swap',
+                },
+            ],
         },
-      ],
     },
-  },
+    icon: {
+        serverBundle: {
+            collections: ['lucide', 'simple-icons']
+        }
+    },
 })
